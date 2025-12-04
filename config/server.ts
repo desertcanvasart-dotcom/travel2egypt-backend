5786c853137ec53c51cd6f79a46ddb05d669d992
boussa69 <islamjp69@gmail.com>
2025-12-04 21:10:02 +0200

    fix: add APP_KEYS fallback

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,11 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [
+      'cSHsGTm5yuGG2s3YSnedlw==',
+      'JnAY+dyNZDTSwCGVdWGCQg==',
+      'oeejoKsBFPAOTCMxxng6bQ==',
+      'd5tLgrvTMhW2MC7P3nmHuQ=='
+    ]),
   },
 });
